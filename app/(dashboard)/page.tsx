a001ae2c5348172ca0d1e20a6bc37a32044568f4
Panudach Kongsomboon <120073076+AumPanudach@users.noreply.github.com>
2024-09-08 21:42:57 +0700

    update authentication

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -2,8 +2,8 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
-      <Button variant="destructive">click me</Button>
-    </div>
+    <p>
+      this is an authentication
+    </p>
   );
 }
