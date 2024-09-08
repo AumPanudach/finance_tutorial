a343fa8fb5e0ca41434a77102814de3b651c03c5
Panudach Kongsomboon <120073076+AumPanudach@users.noreply.github.com>
2024-09-08 21:54:28 +0700

    update

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,14 +1,23 @@
-import { SignIn } from "@clerk/nextjs";
-
+import { SignIn, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
+import { Loader2 } from "lucide-react";
 export default function Page() {
   return (
     <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
-      <div className="h-full lg:flex flex-col items-center justify-center 4px">
-        <div className="text-center space-y-4 pt-14">
+      <div className="h-full lg:flex flex-col items-center justify-center px-4">
+        <div className="text-center space-y-4 pt-16">
           <h1 className="font-bold text-3xl text-[#2E2A47]">Welcome back</h1>
-          <p>Log in or Create account to get back your dashboard</p>
+          <p className="text-base text-[#7E8CA0]">
+            Log in or Create account to get back your dashboard
+          </p>
         </div>
-        <SignIn />
+        <div className="flex items-center justify-center mt-8">
+          <ClerkLoaded>
+            <SignIn />
+          </ClerkLoaded>
+          <ClerkLoading>
+            <Loader2 className="animate-spin text-muted-foreground" />
+          </ClerkLoading>
+        </div>
       </div>
     </div>
   );
