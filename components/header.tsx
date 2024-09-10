1884364b78243cb67950bdc33628e233144f3520
Panudach Kongsomboon <120073076+AumPanudach@users.noreply.github.com>
2024-09-10 07:59:02 +0700

    update  header logo

--- components/header.tsx
+++ components/header.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import HeaderLogo from "@/components/header-logo";
 
 const header = () => {
   return (
-    <header className='bg-gradient-to-b'>Header</header>
-  )
-}
+    <header className="bg-gradient-to-b from-blue-700 to-blue-500 px-4 py-8 lg:px-14 pb-36">
+      <div className="max-w-screen-2xl mx-auto">
+        <div className="w-full flex items-center justify-between mb-14">
+          <div className="flex items-center lg:gap-x-16">
+            <HeaderLogo />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
 
-export default header
+export default header;
